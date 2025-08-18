2a01f5e17e6fbfb585baf30cde5489ac7ea7f2b2
APapeIsName <gms72901217@gmail.com>
2025-08-18 21:59:34 +0900

    10.간단한 주문 조회 v1: 엔티티를 직접 노출

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne 관계 (ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); // Lazy 강제 초기화됨 - getMember 는 프록시, getName으로 한 번 더 부르면 Lazy가 강제 초기화됨
+            order.getDelivery().getAddress(); // Lazy 강제 초기화됨
+        }
+        return all;
+    }
+
+}
